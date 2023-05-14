4eeea92970118521acededbcecb09ac79d8009bd
giahuybui <21211tt4913@mail.tdc.edu.vn>
2023-05-14 22:11:58 +0700

    App this way

---
+++ Project_MapTests/Project_MapTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_MapTests.swift
+//  Project_MapTests
+//
+//  Created by CNTT on 5/14/23.
+//  Copyright © 2023 fit.tdc. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Map
+
+class Project_MapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
